28cddf58748e9d367cbec8ed23057de36f49f742
alex3016 <a0078102@nus.edu.sg>
2013-10-13 00:18:50 +0800

    bullet roughly finished

--- classes/bullet.js
+++ classes/bullet.js
@@ -1,23 +1,65 @@
+"use strict";
 
-function bullet(stage,init_x,init_y,speed,path) {
-	var that = this;
-	var stage = stage;
-	var texture = PIXI.Texture.fromImage(path);
-	var b = new PIXI.Sprite(texture);
-	b.anchor.x = 0.5;
-    b.anchor.y = 0.5;
+function bullet(stageArg, init_x, init_y, speed, path, isS) {
+    var wallDetector;
+    var that = this;
+    var stage = stageArg;
+    var isServer = isS;
+    var x = init_x + speed * 2;
+    var y = init_y;
+    var onStage = false;
+    if (!isServer)
+    {
+        var texture = PIXI.Texture.fromImage(path);
+        var b = new PIXI.Sprite(texture);
+        b.anchor.x = 0.5;
+        b.anchor.y = 0.5;
+
+        b.position.x = x;
+        b.position.y = y;
+        stage.addChild(b);
+        onStage = true;
+    }
     this.damage = 5;
-	this.ttl=18;
-	this.speed = speed;
-	b.position.x=init_x;
-	b.position.y=init_y;
-	var tex=texture;
-	stage.addChild(b);
-	this.move = function(){
-		b.position.x+=speed;
-		that.ttl--;
-		if(that.ttl==0)
-			stage.removeChild(b);
-	}
-	
-};
+    this.ttl = 18;
+    this.speed = speed;
+    wallDetector = new Rectangle(stage, x - 42, y - 6, 84, 13);
+    this.getRect = function() {
+        return wallDetector;
+    }
+    this.getDamage = function() {
+        return that.damage;
+    }
+    this.move = function() {
+        wallDetector.moveX(speed);
+        x += speed;
+        if (!isServer)
+            b.position.x = x;
+        if (that.ttl > 0)
+            that.ttl--;
+
+        for (var i = 0; i < mapRects.length; i++)
+            if (wallDetector.isIntersecting(mapRects[i]))
+            {
+                that.ttl = 0;
+                break;
+            }
+        if (that.ttl == 0 && !isServer)
+        {
+            stage.removeChild(b);
+            wallDetector.destroy();
+            onStage = false;
+        }
+    }
+    this.destroy = function() {
+        that.ttl = 0;
+        if (onStage)
+        {
+            stage.removeChild(b);
+            wallDetector.destroy();
+            onStage = false;
+        }
+    }
+}
+if (typeof window == 'undefined')
+    global.bullet = bullet;
